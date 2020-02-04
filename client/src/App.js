3554edfb197068588f62abe913714e0ededcc629
Tristan Depew <tristan.depew@gmail.com>
2020-02-04 10:11:37 -0500

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import BubblePage from "./components/BubblePage";
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -9,10 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <ProtectedRoute path="/bubbles" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    username: "Lambda School",
+    password: "i<3Lambd4"
+  });
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:5000/api/login", user)
+      .then(reponse => {
+        console.log("logged in", reponse.data.payload);
+
+        localStorage.setItem("token", reponse.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(error => {
+        console.log("err", error);
+      });
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="loginForm">
+      <form name="login">
+        <div className="loginInputContainer">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="loginInputContainer">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={handleSubmit}>Log in</button>
+      </form>
+    </div>
   );
 };
 
